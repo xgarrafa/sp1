e289230767e8d1d7047c5a05ab35b00759fb266d
tamirhemo <tamirhmo@gmail.com>
2026-03-18 22:51:13 +0000

    example

--- examples/poseidon2/program/src/main.rs
+++ examples/poseidon2/program/src/main.rs
@@ -1,26 +1,19 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use sp1_zkvm::syscalls::syscall_poseidon2;
 use sp1_zkvm::syscalls::Poseidon2State;
 
 pub fn main() {
-    let p = Poseidon2State([0; 16]);
-    syscall_poseidon2(&p);
+    let mut state = Poseidon2State::default();
 
-    let p = Poseidon2State([1; 16]);
-    syscall_poseidon2(&p);
-
-    let p = Poseidon2State([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
-    syscall_poseidon2(&p);
-
-    let p = Poseidon2State([0, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0]);
-    syscall_poseidon2(&p);
-
-    let p = Poseidon2State([10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160]);
-    for _ in 0..1000000 {
-        syscall_poseidon2(&p);
+    // Hash ~10 MB: 436906 blocks of 24 bytes each = 10,485,744 bytes.
+    for i in 0u32..436906 {
+        let mut block = [0u8; 24];
+        let bytes = i.to_le_bytes();
+        block[..4].copy_from_slice(&bytes);
+        state.absorb_byte_block(&block);
     }
 
-    println!("successfully evaluated poseidon2");
+    let output = state.output();
+    println!("poseidon2 hash: {:?}", output);
 }
