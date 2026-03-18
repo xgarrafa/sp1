1e44000d5e31929a4eda1b95e6bc7fc68185d071
tamirhemo <tamirhmo@gmail.com>
2026-03-18 14:07:55 +0000

    byte only

--- crates/zkvm/lib/src/poseidon2.rs
+++ crates/zkvm/lib/src/poseidon2.rs
@@ -2,15 +2,17 @@ const WIDTH: usize = 16;
 const RATE: usize = 8;
 const BYTE_BLOCK_SIZE: usize = 24;
 
-use std::process::Output;
-
-use elliptic_curve::ff::derive::bitvec::field;
-
 use crate::syscall_poseidon2;
 
 #[repr(C)]
 #[repr(align(8))]
 pub struct Poseidon2State([u32; WIDTH]);
+
+impl Default for Poseidon2State {
+    fn default() -> Self {
+        Self([0; WIDTH])
+    }
+}
 
 impl Poseidon2State {
     #[inline]
@@ -25,12 +27,17 @@ impl Poseidon2State {
         self.0.as_mut_ptr()
     }
 
-    fn absorb_field_block(&mut self, block: &[u32; RATE]) {
+    /// Absorb a [`RATE`] size block of field elements
+    ///
+    /// # Safety
+    /// This function assumes that the elements are within the `SP1Field` range. Breaking this
+    /// constraint will lead to prover panic.
+    unsafe fn absorb_field_block_unchecked(&mut self, block: &[u32; RATE]) {
         self.0[0..RATE].copy_from_slice(block);
         self.permute();
     }
 
-    pub fn absorb_block(&mut self, block: &[u8; BYTE_BLOCK_SIZE]) {
+    pub fn absorb_byte_block(&mut self, block: &[u8; BYTE_BLOCK_SIZE]) {
         let mut field_block = [0u32; RATE];
         // Accumulate every 24 bytes to a field element.
         for (i, element) in field_block.iter_mut().enumerate() {
@@ -39,51 +46,14 @@ impl Poseidon2State {
             *element += (block[start_idx + 1] as u32) << 8;
             *element += (block[start_idx + 1] as u32) << 16;
         }
-        self.absorb_field_block(&field_block);
+        unsafe {
+            self.absorb_field_block_unchecked(&field_block);
+        }
     }
 
     pub fn output(self) -> [u32; RATE] {
         let mut output = [0; RATE];
         output.copy_from_slice(&self.0[0..RATE]);
         output
-    }
-}
-
-struct DuplexSponge {
-    state: Poseidon2State,
-    absorbing_index: usize,
-    squeezing_index: usize,
-}
-
-impl DuplexSponge {
-    fn new() -> Self {
-        let state = Poseidon2State([0; WIDTH]);
-
-        Self { state, absorbing_index: 0, squeezing_index: RATE }
-    }
-
-    #[inline]
-    fn absorb(&mut self, element: u32) {
-        self.absorbing_index += 1;
-        if self.absorbing_index == RATE {
-            unsafe {
-                syscall_poseidon2(&mut self.state);
-            }
-            self.absorbing_index = 0;
-        }
-        self.state.0[self.absorbing_index] = element;
-    }
-
-    #[inline]
-    fn squeeze(&mut self) -> u32 {
-        self.absorbing_index = 0;
-        if self.squeezing_index == RATE {
-            unsafe {
-                syscall_poseidon2(&mut self.state);
-            }
-            self.squeezing_index = 0;
-        }
-
-        0
     }
 }
